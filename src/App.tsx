403f4d3fc7406e2845cbab2a07ed2132736a4da6
Marcia <marcialeite483@gmail.com>
2025-12-13 18:23:06 -0300

    chore: initialize project with Vite, TailwindCSS and shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <>
+      <Button>Click Me!</Button>
+      <h1 className="text-red-500 p-4">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
